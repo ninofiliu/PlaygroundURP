cb78738ab83eb3361912b4dcff2f3eedea307ae1
Nino Filiu <2unfshwpu@mozmail.com>
2025-05-01 19:38:35 +0200

    move shit to stuff

--- Assets/net/GameStuff.cs
+++ Assets/net/GameStuff.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class GameStuff : NetworkBehaviour {
   public TMP_InputField playerIdInput;
 
+  void Start() {
+    if (Application.isBatchMode) {
+      Debug.Log("Server env detected, starting NGO server");
+      NetworkManager.Singleton.StartServer();
+    }
+  }
+
+  public void Host() {
+    Debug.Log("Starting host...");
+    NetworkManager.Singleton.StartHost();
+  }
+
+  public void Join() {
+    Debug.Log("Joining...");
+    NetworkManager.Singleton.StartClient();
+  }
+
   public void SetPlayerId() {
     var player =
         NetworkManager.LocalClient.PlayerObject.GetComponent<PlayerStuff>();
