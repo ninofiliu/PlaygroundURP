a89d1ce15881caa8c130f75e65ac7c03394fe679
Nino Filiu <2unfshwpu@mozmail.com>
2025-05-01 18:01:02 +0200

    server mode

--- Assets/net/NetworkManagerButtons.cs
+++ Assets/net/NetworkManagerButtons.cs
@@ -1,7 +1,14 @@
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
 public class NetworkManagerButtons : MonoBehaviour {
+  void Start() {
+    if (Application.isBatchMode) {
+      Debug.Log("Server env detected, starting NGO server");
+      NetworkManager.Singleton.StartServer();
+    }
+  }
 
   public void Host() {
     Debug.Log("Starting host...");
